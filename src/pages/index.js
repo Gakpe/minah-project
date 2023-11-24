006c6406ca0414126ed6bff54a1cf2a848389ec5
dolat-hamza <dolathamza8019@gmail.com>
2023-11-24 05:11:30 +0500

    Initial Push

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import Image from 'next/image'
+import {Inter} from 'next/font/google'
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import Footer from "@/components/Footer";
+
+const inter = Inter({subsets: ['latin']})
+
+export default function Home() {
+    return (
+        <div>
+            <Navbar/>
+            {/*<HeroSection/>*/}
+            {/*<Footer/>*/}
+        </div>
+
+
+    )
+}
