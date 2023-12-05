47ca88b6dc52f1382d24c8a6c85da6c2cc539669
dolathamza <dolathamza8019@gmail.com>
2023-12-05 19:22:53 +0500

    Added Apple, Google, and other Logins with backend integration done

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -1,5 +1,6 @@
 // pages/api/login.js
 import {Magic} from '@magic-sdk/admin';
+import {postToken} from "../../../util";
 
 // Create an instance of magic admin using our secret key (not our publishable key)
 let mAdmin = new Magic(process.env.MAGIC_SECRET_KEY);
@@ -11,6 +12,11 @@ export default async function login(req, res) {
             req.headers.authorization,
         );
         console.log("here is did token :", didToken)
+        postToken(didToken).then((res)=>{
+            console.log(res)
+        }).catch((err)=>{
+            console.log(err)
+        }
         // Validate the token and send back a successful response
         await mAdmin.token.validate(didToken);
         res.status(200).json({data: didToken});
